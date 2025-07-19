a50b765f16c6e8ec2ff3a6f2492f30fa64cc088e
Darker <bhosalesudarshan9@gmail.com>
2025-07-19 14:08:04 +0530

    Addition: user schema

--- models/User.js
+++ models/User.js
@@ -7,14 +7,18 @@ const schema = new mongoose.Schema(
     firstname: {
       type: String,
       trim: true,
+    }, 
+    lastname: {
+      type: String,
+      trim: true,
     },
     role: {
       type: String,
       default: "user",
     },
-    lastname: {
+    employeeId: {
       type: String,
-      trim: true,
+      trim: true
     },
     email: {
       type: String,
@@ -22,40 +26,29 @@ const schema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    tokens: {
-      type: [String],
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-    OTP: { type: String },
+    
+    
   },
   {
     timestamps: true,
   }
-);
+)
 
-schema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// schema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
 
-schema.methods.matchPassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
+// schema.methods.matchPassword = async function (password) {
+//   return await bcrypt.compare(password, this.password);
+// };
 
-schema.methods.getToken = async function () {
-  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
-};
+// schema.methods.getToken = async function () {
+//   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+//     expiresIn: process.env.JWT_EXPIRE,
+//   });
+// };
 
 module.exports = mongoose.model("User", schema);
